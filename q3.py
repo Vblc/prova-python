b112a68ba6ae98ad071fa3d0de4de650b67e25da
Vblc <79917734+Vblc@users.noreply.github.com>
2022-03-30 12:41:02 -0300

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,16 +1,24 @@
 # Pergunte o valor de "n" para o usuário e salve-o como uma variável inteira.
-n = ...
+n = int(input("n: "))
 
 # Calcule a sequência de Collatz para esse número
 seq = [n]
-while ...:
-    ...
+passos = 1
+numeros = []
+while n != 1:
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = 3*n + 1
+    passos = passos +1
+    numeros.append(n)
+
 print("seq =", seq)
 
 # Calcule quantos termos possui a sequência obtida
-n_termos = ...
-print("n termos =", n_termos)
+n_termos = passos
+print("n termos =", passos)
 
 # Diga qual é o maior valor atingido nesta sequência.
-maior = ...
+maior = max(numeros)
 print("maior valor =", maior)
