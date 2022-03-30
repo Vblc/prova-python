83eec71d7c3471ceba2d706b7f4e340453171323
Vblc <79917734+Vblc@users.noreply.github.com>
2022-03-30 12:08:38 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,7 +1,7 @@
 """
 Explique no comentário porque o código do exemplo estava incorreto:
 
-
+R: o progama falha pois podem ocorrer vários if ao mesmo tempo, causando respostas dupla.
 
 
 
@@ -9,22 +9,29 @@ Explique no comentário porque o código do exemplo estava incorreto:
 
 # Substitua os valores de A, B e C e modifique o código para corrigir
 # os erros
-A = ...
-B = ...
-C = ...
+
+A = 211
+B = 62
+C = 606
 minABC = min(A, B, C)
 maxABC = max(A, B, C)
-
+n = 0
 x = int(input("x: "))
+
 if x == 0:
     print("nulo")
-elif x == 42:
-    print("resposta para *a* pergunta")
-elif x > maxABC:
-    print("grande")
+    n = n + 1
 if x < 0:
     print("negativo")
+    n = n + 1
+elif x == 42:
+    print("resposta para *a* pergunta")
+    n = n + 1 
+elif x > maxABC:
+    print("grande")
+    n = n + 1
 if minABC < x < maxABC:
     print("médio")
-else:
+    n = n + 1
+if n == 0:
     print("nada de especial")
