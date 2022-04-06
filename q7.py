7ba9dd4781c25adb4adb04b620579b29dcd0620c
Vblc <79917734+Vblc@users.noreply.github.com>
2022-04-06 20:58:55 -0300

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,6 +1,14 @@
 def mdc(x, y):
-    return ...
-
+    
+    if x > y:
+        small = y
+    else:
+        small = x
+    for i in range(1, small+1):
+        if((x % i == 0) and (y % i == 0)):
+            gcd = i
+              
+    return gcd
 
 if __name__ == "__main__":
     x = int(input("x: "))
